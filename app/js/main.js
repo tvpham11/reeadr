7cc13f45d4f51706511ae3ebc54b789bd9112288
Vi Pham <tvpham11@gmail.com>
2015-06-25 09:56:57 -0400

    controller as example

--- app/js/main.js
+++ app/js/main.js
@@ -2,37 +2,12 @@
 
   'use strict';
 
-  angular.module('Reeadr', ['ngRoute'])
+  angular.module('Reeadr', [])
 
-  .constant('PARSE', {
+  .controller('BooksList', [
 
-    URL: 'https://api.parse.com/1/',
-    CONFIG: {
-      headers: {
-        'X-Parse-Application-Id': 'pztDT2ZdNHGo1nxnUgVOjWgBWaHoFxamvPeg90b7',
-        'X-Parse-REST-API-Key'  : '9mXq8eh2TWSZf4A63znE96iJsaVfaLJOHAgkskuL'
-      }
-    }
-  })
-
-  .config( [ '$routeProvider',
-    function($routeProvider) {
-      $routeProvider.when('/', {
-        controller: 'BooksList',
-        template: 'Hello from {{ pageName }}'
-      })
-
-      .when('/about', {
-        controller: 'BooksList',
-        template: 'Hello from about page'
-      });
-    }
-
-  ])
-
-  .controller('BooksList', ['$scope',
-    function($scope) {
-      $scope.pageName = 'right here!!';
+    function() {
+      this.pageName = 'New Name';
     }
 
   ]);
